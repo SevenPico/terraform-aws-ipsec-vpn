e5f01646be919a2dbbe84c642313d26c09ac630f
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-06-16 00:42:22 +0530

    policy documents

--- iam.tf
+++ iam.tf
@@ -156,7 +156,7 @@ module "ec2_autoscale_group_role" {
   permissions_boundary  = ""
   policy_description    = "VPN Server Permissions"
   policy_document_count = 1
-  policy_documents      = data.aws_iam_policy_document.ec2_autoscale_group_role_policy.*.json
+  policy_documents      = try(data.aws_iam_policy_document.ec2_autoscale_group_role_policy[*].json, [])
   principals = {
     Service : [
       "ec2.amazonaws.com",
